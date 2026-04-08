2ffe22320a03bc475ae8eed2d3780204997368a4
滝口将史 <m4masashi@takiguchimasashinoMac-mini.local>
2026-04-08 22:32:24 +0900

    Firebase連携追加・firebase-config.js公開

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebaseのプレースホルダ設定ファイル
+// ご自身でFirebaseプロジェクトを作成し、以下の値を実際のキーに置き換えてください。
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDN6A8dTYXhf0jjC0lZAfy9WRCchYbIGJQ",
+  authDomain: "jabra-board.firebaseapp.com",
+  projectId: "jabra-board",
+  storageBucket: "jabra-board.firebasestorage.app",
+  messagingSenderId: "894113977769",
+  appId: "1:894113977769:web:09253f6eed96c7675860b8"
+};
